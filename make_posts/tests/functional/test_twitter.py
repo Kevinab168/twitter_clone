02a158f1dc6af65d22bc6959644a103fe47ca805
Kevin <kevin.abraham1688@gmail.com>
2020-07-17 12:55:40 -0400

    Add login page test
    - Create fixture to login programatically
    - Create UserFactory
    - Create User model

--- make_posts/tests/functional/test_twitter.py
+++ make_posts/tests/functional/test_twitter.py
@@ -1,4 +1,28 @@
+from pytest_factoryboy import register
+from make_posts.tests.factories import UserFactory
+
+register(UserFactory)
+
 def test_landing_page(driver, live_server):
     driver.get(live_server.url)
     assert "welcome" in driver.page_source.lower()
     assert driver.find_element_by_css_selector('[data-test="log_in"]')
+
+
+def test_login_page(driver, live_server, user_factory):
+    # user = user_factory.create(password='hello')
+    user = user_factory.create()
+    user.set_password('test')
+    user.save()
+    driver.get(live_server.url + '/login')
+    username_input = driver.find_element_by_css_selector('[data-test="username"]')
+    username_input.send_keys(user.username)
+    password_input = driver.find_element_by_css_selector('[data-test="password"]')
+    password_input.send_keys('test')
+    log_in = driver.find_element_by_css_selector('[data-test="log-in"]')
+    log_in.click()
+    assert user.username in driver.find_element_by_css_selector('[data-test="user"]').text
+    assert 'followers' in driver.page_source
+    assert 'following' in driver.page_source
+    assert 'posts' in driver.page_source
+    assert driver.find_element_by_css_selector('[data-test="make-post"]')
