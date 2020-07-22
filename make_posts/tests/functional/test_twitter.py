6611c4258ee1aa2068ad11e31c34042e050ab3f7
Kevin <kevin.abraham1688@gmail.com>
2020-07-22 13:54:03 -0400

    Modify tests to use last element instead of first

--- make_posts/tests/functional/test_twitter.py
+++ make_posts/tests/functional/test_twitter.py
@@ -48,8 +48,8 @@ def test_make_comment(driver, live_server, login_user, user_factory, post_factor
     login_user(user)
     for _ in range(10):
         post_factory.create(user=user)
-    post_1 = Post.objects.get(pk=1)
-    driver.get(live_server + f'/posts/{post_1.pk}')
+    post = Post.objects.all().last()
+    driver.get(live_server + f'/posts/{post.pk}')
     comment_field = driver.find_element_by_css_selector('[data-test="comment-text"]')
     comment_field.send_keys('Sample1')
     submit_comment = driver.find_element_by_css_selector('[data-test="create-comment"]')
@@ -57,7 +57,7 @@ def test_make_comment(driver, live_server, login_user, user_factory, post_factor
     assert 'Sample1' in driver.find_element_by_css_selector('[data-test="user-comment"]').text
     recent_comment = Comment.objects.all().last()
     assert recent_comment.user == user
-    assert recent_comment.post == post_1
+    assert recent_comment.post == post
 
 
 def test_follow_change(driver, live_server, login_user, user_factory, post_factory):
@@ -86,8 +86,8 @@ def test_see_followers(driver, live_server, login_user, user_factory, follow_fac
     followers_count = 20
     assert len(followers_list) == followers_count
     assert 'Followers' in driver.find_element_by_css_selector('[data-test="follower-page-heading"]').text
-    first_follower = Follow.objects.get(pk=1)
-    assert first_follower.following == user
+    last_follower = Follow.objects.all().last()
+    assert last_follower.following == user
 
 
 def test_go_to_follower_homepage(driver, live_server, login_user, user_factory, follow_factory):
@@ -96,10 +96,10 @@ def test_go_to_follower_homepage(driver, live_server, login_user, user_factory, 
     for _ in range(20):
         follow_factory.create(following=user)
     driver.get(live_server.url + f'/users/{user.pk}/followers')
-    follower_homepage = driver.find_element_by_css_selector('[data-test="follower-homepage"]')
+    follower_homepage = driver.find_elements_by_css_selector('[data-test="follower-homepage"]')[-1]
     follower_homepage.click()
-    first_follower = Follow.objects.get(pk=1)
-    assert first_follower.follower.username in driver.find_element_by_css_selector('[data-test="user"]').text
+    last_follower = Follow.objects.all().last()
+    assert last_follower.follower.username in driver.find_element_by_css_selector('[data-test="user"]').text
     assert driver.find_element_by_css_selector('[data-test="follower-count"]')
     assert driver.find_element_by_css_selector('[data-test="following-count"]')
 
@@ -114,8 +114,8 @@ def test_see_following(driver, live_server, login_user, user_factory, follow_fac
     following_count = 20
     assert len(following_list) == following_count
     assert 'Following' in driver.find_element_by_css_selector('[data-test="following-page-heading"]').text
-    first_user_followed = Follow.objects.get(pk=1)
-    assert first_user_followed.follower == user
+    last_user_followed = Follow.objects.all().last()
+    assert last_user_followed.follower == user
 
 
 def test_go_to_following_homepage(driver, live_server, login_user, user_factory, follow_factory):
@@ -124,10 +124,10 @@ def test_go_to_following_homepage(driver, live_server, login_user, user_factory,
     for _ in range(20):
         follow_factory.create(follower=user)
     driver.get(live_server.url + f'/users/{user.pk}/following')
-    following_homepage = driver.find_element_by_css_selector('[data-test="following-homepage"]')
+    following_homepage = driver.find_elements_by_css_selector('[data-test="following-homepage"]')[-1]
     following_homepage.click()
-    first_user_followed = Follow.objects.get(pk=1)
-    assert first_user_followed.following.username in driver.find_element_by_css_selector('[data-test="user"]').text
+    last_user_followed = Follow.objects.all().last()
+    assert last_user_followed.following.username in driver.find_element_by_css_selector('[data-test="user"]').text
     assert driver.find_element_by_css_selector('[data-test="follower-count"]')
     assert driver.find_element_by_css_selector('[data-test="following-count"]')
 
